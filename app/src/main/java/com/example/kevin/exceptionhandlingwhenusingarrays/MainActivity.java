90ea9f64982cd9c68e6937edbce6c21bcb9a3cd5
kevin <kevinjobrien420@gmail.com>
2018-06-13 18:06:38 -0500

    191 ExceptionHandlingWhenUsingArrays //Coding

--- app/src/main/java/com/example/kevin/exceptionhandlingwhenusingarrays/MainActivity.java
+++ app/src/main/java/com/example/kevin/exceptionhandlingwhenusingarrays/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.kevin.exceptionhandlingwhenusingarrays;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtRates = (TextView)findViewById(R.id.txtRates);
+        TextView txtNumberOfStarts = (TextView)findViewById(R.id.txtNumberOfStars);
+        String oldTxtRatesValue;
+        String oldTxtNumberOfStarsValue;
+
+        int[] peopleRates = {1, 4, 3, 5, 2, 5, 1, 3, 4, 5, 2, 4, 5};
+
+
+
+
+
+
     }
 }
